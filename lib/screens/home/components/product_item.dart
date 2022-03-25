622681185f7e30431e4acee7214b7923067fd692
Choi Yoonje <choiyoonje@MacBook-Pro.local>
2022-03-25 21:52:00 +0900

    product item

---
+++ lib/screens/home/components/product_item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:todos/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({
+    required this.product,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 135.0,
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image.network(
+              product.urlToImage,
+              width: 115,
+              height: 115,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(
+            width: 16.0,
+          ),
+          // ProductDetail(product:product)
+        ],
+      ),
+    );
+  }
+}
